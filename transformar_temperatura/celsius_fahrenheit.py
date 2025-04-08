1a54230b390136bf079fe5ce7b3e89369b7ee8ff
livia-hermano <livia.hermano@aluno.senai.br>
2025-04-08 09:50:46 -0300

    incluir2
    incluir2 completo mas informações erradas

--- transformar_temperatura/celsius_fahrenheit.py
+++ transformar_temperatura/celsius_fahrenheit.py
@@ -1,8 +1,13 @@
-print("Vamos converter a temperatura para CELSIUS ou FAHRENHEIT")
-def celsius_fahrenheit():
+print("Vamos mudar a marca para KELVIN ou KLEIN")
+def kelvin_klein():
+    temperatura = int(input("Digite o valor da marca: "))
     opcao = input("""Digite a opção de conversão:
-                C - Celsius para fahrenheit
-                f - fahrenheit para celcius: """)
+                ke - kelvin para klein
+                kl - klein para kelvin: """)
     if opcao == 'f' or opcao == 'F':
+        resultado = (temperatura - 32) * (5/9)
+        print("A temperatura convertida é de:",resultado)
     elif opcao == 'c' or opcao == 'C':
-celsius_fahrenheit()
+        resultado = temperatura * 9/5 + 32 
+        print("A temperatura convertida é de:",resultado)
+kelvin_klein()
