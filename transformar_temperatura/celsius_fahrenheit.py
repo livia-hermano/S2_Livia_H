0faecfbd63b323c5e9a69b2ca770cf3f698b8036
livia-hermano <livia.hermano@aluno.senai.br>
2025-04-08 09:03:29 -0300

    main criada
    linhas de código incompletas

---
+++ transformar_temperatura/celsius_fahrenheit.py
@@ -0,0 +1,8 @@
+print("Vamos converter a temperatura para CELSIUS ou FAHRENHEIT")
+def celsius_fahrenheit():
+    opcao = input("""Digite a opção de conversão:
+                C - Celsius para fahrenheit
+                f - fahrenheit para celcius: """)
+    if opcao == 'f' or opcao == 'F':
+    elif opcao == 'c' or opcao == 'C':
+celsius_fahrenheit()
